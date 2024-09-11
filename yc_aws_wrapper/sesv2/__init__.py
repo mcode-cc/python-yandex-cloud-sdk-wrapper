12f6ca854be4c24c3cdd68121e8a1e9f4f9af5ce
Zhdanov-Vyacheslav <slava.zhdanov@phkp.ru>
2024-09-11 11:57:37 +0300

    Added the ability to transfer content according to the documentation of boto3

--- yc_aws_wrapper/sesv2/__init__.py
+++ yc_aws_wrapper/sesv2/__init__.py
@@ -11,29 +11,28 @@ class SESV2Client(DynamicClient):
     def __init__(self, client: boto3.client, path: str):
         super().__init__(client, path)
 
-    def send(self, to: Union[str, List[str]], title, message, charset: str = "UTF-8"):
-        destination = {
-            "ToAddresses": [to] if isinstance(to, str) else to
-        }
-        simple = {
-            "Subject": {
-                "Data": title,
-                "Charset": charset
-            },
-            "Body": {
-                "Text": {
-                    "Data": message,
-                    "Charset": charset
+    def send(self, to: Union[str, List[str]], title, message, charset: str = "UTF-8", **kwargs):
+        destination = kwargs["Destination"] if "Destination" in kwargs else {}
+        destination["ToAddresses"] = [to] if isinstance(to, str) else to
+        content = kwargs["Content"] if "Content" in kwargs else {}
+        if len(content) < 1:
+            content = {
+                "Simple": {
+                    "Subject": {"Data": title, "Charset": charset},
+                    "Body": {"Text": {"Data": message, "Charset": charset}}
                 }
-            },
-        }
+            }
+        elif "Simple" in content:
+            content["Simple"]["Subject"] = {"Data": title, "Charset": charset}
+            if "Body" not in content["Simple"]:
+                content["Simple"]["Body"] = {}
+            if "Html" not in content["Simple"]["Body"]:
+                content["Simple"]["Body"]["Text"] = {"Data": message, "Charset": charset}
         try:
             return self.client.send_email(
                 FromEmailAddress=self.path,
                 Destination=destination,
-                Content={
-                    "Simple": simple
-                }
+                Content=content
             )
         except ClientError as e:
             if boto_exception(e, ""):
